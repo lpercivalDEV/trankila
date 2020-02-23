6de0fdef9c84a411ff10f5b6f60df7d5ba0b7ab7
lpercivalDEV <lpercival.dev@gmail.com>
2020-02-22 20:51:56 -0500

    Add sound and time selection logic

--- main.js
+++ main.js
@@ -30,6 +30,16 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  //select different sounds
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function(){
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    })
+  });
+
+
   //play sound
   play.addEventListener("click", ()=> {
     checkPlaying(song);
